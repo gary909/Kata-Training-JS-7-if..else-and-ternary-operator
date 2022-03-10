b09661f8739c71ada05c5585779c7f8db4220a57
gary <gary.white@lodgit.com>
2022-03-10 13:08:34 +0100

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function saleHotdogs(n){
+    return ?;
+}
+
+console.log(saleHotdogs(1)); // 100
+console.log(saleHotdogs(4)); // 400
+console.log(saleHotdogs(5)); // 475
+console.log(saleHotdogs(9)); // 855
+console.log(saleHotdogs(10)); // 900
+console.log(saleHotdogs(100)); // 9000
